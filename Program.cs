ef889df8a0c82d06a0e95aeb03434fb664b98d83
NateRomans <nromans1@ivytech.edu>
2025-03-22 21:19:41 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+namespace BoxerWeightClassification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the boxer's weight in pounds: ");
+            double weight = Convert.ToDouble(Console.ReadLine());
+
+            string classification = GetWeightClassification(weight);
+
+            Console.WriteLine($"The boxer's weight classification is: {classification}");
+        }
+
+        static string GetWeightClassification(double weight)
+        {
+            if (weight > 190) return "Heavyweight";
+            else if (weight > 175) return "Cruiserweight";
+            else if (weight > 168) return "Light Heavyweight";
+            else if (weight > 160) return "Super Middleweight";
+            else if (weight > 154) return "Middleweight";
+            else if (weight > 147) return "Junior Middleweight";
+            else if (weight > 140) return "Welterweight";
+            else return "Junior Welterweight";
+        }
+    }
+}
